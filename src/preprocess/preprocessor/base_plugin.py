9acd616e8ab601bac094060f0fdf25e2f548ef78
Muhammad Nabi Yasinzai <muhammad.nabi.yasinzai@gmail.com>
2024-11-20 11:37:04 +1300

    Moved Preprocessor Logger Configs to based Plugin

--- src/preprocess/preprocessor/base_plugin.py
+++ src/preprocess/preprocessor/base_plugin.py
@@ -1,7 +1,9 @@
 # src/preprocess/preprocessor/base_plugin.py
 
-from abc import ABC, abstractmethod
 from pathlib import Path
+from abc import ABC, abstractmethod
+
+from utils.logging_setup import configure_session_logging
 
 class PreprocessorPlugin(ABC):
     
@@ -27,6 +29,27 @@ class PreprocessorPlugin(ABC):
         self.dataset_path = dataset_path
         self.mapping = mapping
         self.config = config
+        
+        
+        # Processor Logger configuration
+        self.preprocessor_log_file = self.dataset_path / "preprocess.log"
+        
+        # Delete the existing log file if it exists
+        if self.preprocessor_log_file.exists():
+            try:
+                self.preprocessor_log_file.unlink()
+                self.logger.info(f"Deleted existing log file: {self.preprocessor_log_file}")
+            except Exception as e:
+                self.logger.error(f"Failed to delete log file: {e}")
+                
+        self.preprocessor_logger = configure_session_logging(
+            session_log_file=self.preprocessor_log_file,
+            log_config_path=self.config.logger_config_file,
+            logger_name=f'preprocess.{self.dataset_path.name}',
+        )
+        
+        
+        
 
     @abstractmethod
     def run(self) -> bool:
